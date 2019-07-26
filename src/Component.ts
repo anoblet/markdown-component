434023584efcd7e2e5a9468ff315335fb2751a6e
Andrew Noblet <andrewbnoblet@gmail.com>
2019-07-26 12:11:05 -0400

    Chore: Save

--- src/Component.ts
+++ src/Component.ts
@@ -1,4 +1,4 @@
-import { LitElement, customElement } from "lit-element";
+import { LitElement, customElement, property } from "lit-element";
 
 import Style from "./Style";
 import Template from "./Template";
@@ -7,4 +7,12 @@ import Template from "./Template";
 export class MarkdownComponent extends LitElement {
   public static styles = Style;
   public render = Template.bind(this);
+
+  @property() public src;
+
+  firstUpdated() {
+    async () => {
+      const result = await fetch(this.src);
+    }
+  }
 }
