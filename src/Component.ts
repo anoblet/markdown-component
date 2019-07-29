edaecf956bfec1f48640d50f1a8417c9a9c670b9
Andrew Noblet <andrewbnoblet@gmail.com>
2019-07-29 17:48:26 -0400

    Chore: Save

--- src/Component.ts
+++ src/Component.ts
@@ -19,10 +19,13 @@ export class MarkdownComponent extends BeforeRender(LitElement) {
 
   // Runs off of constructor as an async method. Properties are available!
   async beforeRender() {
-    applyStyle(this, this.inheritedStyles);
     const result = await fetch(this.src).then(response => {
       return response.text();
     });
     this.html = marked(result);
+  }
+
+  firstUpdated() {
+    applyStyle(this, this.inheritedStyles);
   }
 }
